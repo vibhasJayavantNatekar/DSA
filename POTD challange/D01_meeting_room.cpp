8a33ebf2ae5ab663fd79577e6feef5b8425642e4
Vibhas Jayvant Natekar <natekarvibhas@gmail.com>
2026-02-16 21:14:18 +0530

    Started a gfg x npci 60 days potd challamge

---
+++ POTD challange/D01_meeting_room.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+bool attendMeeting(vector<vector<int>> &arr ){
+
+sort(arr.begin(), arr.end());
+
+for(int i =0 ; i < arr.size()-1;i++){
+    if(arr[i][1] > arr[i+1][0]){
+        return false;
+    }
+}
+
+
+return true;
+
+}
+
+
+int main(){
+
+    vector<vector<int>>arr ={ { 1 , 4},{10,15},{7,10}};
+
+    cout<<attendMeeting(arr);
+
+
+
+    return 0;
+}
